b0f55f143088d850d1aab3bea989394a1d3eb26a
crystalbrock619 <65456479+crystalbrock619@users.noreply.github.com>
2020-10-15 18:32:50 -0500

    Build
    Added onclick functions for correct and incorrect answers.

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-/*const scramWords = [ebraablek, iaortcen, esenez, asdn, edimx, upro, udcisnisos, plmyutil, uodsiisni, nnevue, dnwyi];
-const unscramWords = [breakable, reaction, sneeze, sand, mixed, pour, discussion, multiply, insidious, uneven, windy];*/
+const scramWords = {plate:"epatl",breakable:"ebraablek",reaction:"iaortcen", sneeze:"esenez", sand:"asdn", mixed:"edimx", pour:"upro", discussion:"udcisnisos", multiply:"plmyutil", insidious:"uodsiisni", uneven:"nnevue", windy:"dnwyi"};
+function genScramWord(){
+    var genScramWord = Math.round(Math.random()*12);
+    return genScramWord;
+}
+let $score = $("#coinCounter");
+let $gameForm = $("<form>");
+$gameForm.addClass("gameForm") 
+const newLabel = $("<label>");
+newLabel.text("Unscramble these letters to form a word:");
+let newSpan = $("<span>");
+newSpan.addClass("scrambled");
+let answerBox = $("<input>");
+answerBox.attr("type", "text").attr("size", "10").attr("id", "answerBox");
+let generateButton = $("<input>");
+generateButton.attr("type", "button").attr("value", "Get New").attr("id", "generateButton");
+let checkButton = $("<input>");
+checkButton.attr("type", "button").attr("value", "Check").attr("id", "checkButton");
+let result = $("<p>");
+result.attr("id", "result");
 $("#startButton").on("click", function(event){
     event.preventDefault();
     var name = $("#name").val();
     var playerName = $("#playerName");
     playerName.text("Player name: "+name);
     $("#startForm").css("display", "none");
-    let $gameForm = $("<form>");
-    $gameForm.addClass("gameForm") 
-    const newLabel = $("<label>");
-    newLabel.text("Unscramble these letters to form a word:");
-    let newSpan = $("<span>");
-    newSpan.addClass("scrambled").text("epatl");
-    let answerBox = $("<input>");
-    answerBox.attr("type", "text").attr("size", "10").attr("id", "answerBox");
-    const checkButton = $("<input>");
-    checkButton.attr("type", "button").attr("value", "Check").attr("id", "checkButton");
-    let result = $("<p>");
-    result.attr("id", "result");
-    $gameForm.append(newLabel, newSpan, answerBox, checkButton);
-    $("#main").prepend($gameForm);
+    $gameForm.append(newLabel, newSpan, answerBox, result, generateButton);
+    $("#main").append($gameForm);
 });
-$("#checkButton").on("click", function(event){
-    event.preventDefault();
-    
+$(generateButton).on("click", function(event){
+    event.preventDefault;
+    generateButton.css("display", "none");
+    checkButton.appendTo($gameForm);
+    checkButton.css("display", "flex");
+    result.text("");
+    answerBox.val("");
+    var scram = Object.values(scramWords);
+    newSpan.text(scram[genScramWord()]);
+})
+$(checkButton).on("click", function(){
+    let playerAnswer = answerBox.val();
+    var answerKeys = Object.keys(scramWords);
+    var isCorrect = answerKeys.includes(playerAnswer);
+    //let scoreStr = ;
+    let score = parseInt($($score).text());
+    if(isCorrect){
+        result.text("Wow! You got it right!");
+        $score.text((score)+1);
+        generateButton.css("display", "flex");
+        checkButton.css("display", "none");
+    } else{
+        result.text("That's not the word we are looking for. Try again.")
+    }
 })
