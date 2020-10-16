62e96332d4a99613cf2825105f490b40ba18ccb5
crystalbrock619 <65456479+crystalbrock619@users.noreply.github.com>
2020-10-16 18:41:45 -0500

    Broke it with this
    Trying to get [ENTER] to trigger form submission. The code works on the start form but the same code with new labels does not work in the game form. Not fixed yet.

--- script.js
+++ script.js
@@ -1,62 +1,120 @@
-const scramWords = {plate:"epatl",breakable:"ebraablek",reaction:"iaortcen", sneeze:"esenez", sand:"asdn", mixed:"edimx", pour:"upro", discussion:"udcisnisos", multiply:"plmyutil", insidious:"uodsiisni", uneven:"nnevue", windy:"dnwyi"};
-function genScramWord(){
-    var genScramWord = Math.round(Math.random()*12);
-    return genScramWord;
-}
-let $score = $("#coinCounter");
-let $gameForm = $("<form>");
-$gameForm.addClass("gameForm") 
-const newLabel = $("<label>");
-newLabel.text("Unscramble these letters to form a word:");
-let newSpan = $("<span>");
-newSpan.addClass("scrambled");
-let answerBox = $("<input>");
-answerBox.attr("type", "text").attr("size", "10").attr("id", "answerBox");
-let generateButton = $("<input>");
-generateButton.attr("type", "button").attr("value", "Get New").attr("id", "generateButton");
-let checkButton = $("<input>");
-checkButton.attr("type", "button").attr("value", "Check").attr("id", "checkButton");
-let result = $("<p>");
-result.attr("id", "result");
-var $rightSound = $("<audio preload=auto>");
-var $wrongSound = $("<audio preload=auto>");
-$rightSound.attr("src", "https://www.kasandbox.org/programming-sounds/rpg/coin-jingle.mp3");
-$wrongSound.attr("src", "https://www.kasandbox.org/programming-sounds/rpg/giant-no.mp3");
-$("#startButton").on("click", function(event){
-    event.preventDefault();
-    var name = $("#name").val();
-    var playerName = $("#playerName");
-    playerName.text("Player name: "+name);
-    $("#startForm").css("display", "none");
-    $gameForm.append(newLabel, newSpan, answerBox, result, generateButton);
-    $("#main").append($gameForm);
-});
-$(generateButton).on("click", function(event){
-    event.preventDefault;
-    generateButton.css("display", "none");
-    checkButton.appendTo($gameForm);
-    checkButton.css("display", "flex");
-    result.text("");
-    answerBox.val("");
-    var scram = Object.values(scramWords);
-    newSpan.text(scram[genScramWord()]);
-})
-$(checkButton).on("click", function(){
-    let playerAnswer = answerBox.val();
-    var answerKeys = Object.keys(scramWords);
-    var isCorrect = answerKeys.includes(playerAnswer);
-    //let scoreStr = ;
-    let score = parseInt($($score).text());
-    if(isCorrect){
-        $("body").append($rightSound);
-        $rightSound[0].play();
-        result.text("Wow! You got it right!");
-        $score.text((score)+1);
-        generateButton.css("display", "flex");
-        checkButton.css("display", "none");
-    } else{
-        $("body").append($wrongSound);
-        $wrongSound[0].play();
-        result.text("That's not the word we are looking for. Try again.")
+//OBJECT CONTAINING UNSCRAMBLED WORDS AND THEIR SCRAMBLED VALUE
+    const scramWords = {plate:"epatl",breakable:"ebraablek",reaction:"iaortcen", sneeze:"esenez", sand:"asdn", mixed:"edimx", pour:"upro", discussion:"udcisnisos", multiply:"plmyutil", insidious:"uodsiisni", uneven:"nnevue", windy:"dnwyi"};
+//RANDOM NUMBER GENERATOR FUNCTION
+    function genScramWord(){
+        var genScramWord = Math.round(Math.random()*12);
+        return genScramWord;
     }
-})
+    var scram = Object.values(scramWords);
+//BEGIN GLOBAL VARIABLE LIST
+//HEADER VARIABLES
+    let $score = $("#coinCounter");
+//START FORM VARIABLES
+    let $startButton = $("#startButton");
+    let $name = $("#name");
+    let $startForm = $("#startForm");
+//GAME FORM VARIABLES
+    let $gameForm = $("<form>");
+    $gameForm.addClass("gameForm");
+//***************************************************************************** */ 
+    let $newLabel = $("<label>");
+    $newLabel.text("Unscramble these letters to form a word:");
+//****************************************************************************** */
+    let $newSpan = $("<span>");
+    $newSpan.addClass("scrambled");
+//***************************** */
+    let $answerBox = $("<input>");
+    $answerBox.attr("type", "text").attr("size", "10").attr("id", "answerBox");
+//****************************************************************************** */
+    let $checkButton = $("<button>");
+    $checkButton.addClass("checkButton");
+    $checkButton.attr("type", "submit");
+    $checkButton.attr("value", "Check");
+    
+//**************************************************************************************** */
+    let $nextButton = $("<input>");
+    $nextButton.val("Get Next").attr("type", "button").addClass("nextButton").css("display", "none");
+//**************************************************************************************************** */
+    let $result = $("<p>");
+    $result.attr("id", "result");
+//**************************************************************************************************** */
+//SOUND VARIABLES
+    var $rightSound = $("<audio preload=auto>");
+    $rightSound.attr("src", "https://www.kasandbox.org/programming-sounds/rpg/coin-jingle.mp3");
+//**************************************************************************************************** */
+    var $wrongSound = $("<audio preload=auto>");
+    $wrongSound.attr("src", "https://www.kasandbox.org/programming-sounds/rpg/giant-no.mp3");
+//**************************************************************************************************** */
+//TRIGGER START FORM SUBMIT ON ENTER IN NAME FIELD
+    $name.on("keyup", function(e){
+        if(e.enterKey){
+            console.log("I'm Done");
+            e.preventDefault;
+            $startForm.submit();
+        }
+    });
+//TRIGGER START FORM SUBMIT ON START BUTTON CLICK
+    $startButton.click(function(e){
+        console.log("I'm Done");
+        e.preventDefault;
+        $startForm.submit();
+    });
+//START FORM SUBMIT EVENT FUNCTION
+    $startForm.submit(function(e){
+        e.preventDefault();
+        console.log("I'm Done");
+        var name = $name.val();
+        var $playerName = $("#playerName");
+        $playerName.text("Player name: "+name);
+        $startForm.css("display", "none");
+        $gameForm.append($newLabel, $newSpan, $answerBox, $result, $checkButton, $nextButton);
+        $newSpan.text(scram[genScramWord()]);
+        $("#main").append($gameForm);
+    });
+
+//TRIGGER GAME FORM SUBMIT ON ENTER IN ANSWER FIELD
+    $gameForm.on("keydown", function(e){
+        if(e.enterKey){
+            console.log("I'm Done");
+            e.preventDefault;
+            $startForm.submit();
+        }
+    });
+//TRIGGER GAME FORM SUBMIT ON CHECK BUTTON CLICK
+    $checkButton.on("click", function(e){
+        e.preventDefault;
+        console.log("I'm Done");
+        $gameForm.submit();
+    })
+//GAME FORM SUBMIT EVENT FUNCTION
+    $gameForm.on("submit", function(e){
+        e.preventDefault;
+        console.log("I'm Done");
+        let playerAnswer = $answerBox.val();
+        var answerKeys = Object.keys(scramWords);
+        var isCorrect = answerKeys.includes(playerAnswer);
+        let score = parseInt($($score).text());
+        if(isCorrect){
+            console.log("I'm Done");
+            $("body").append($rightSound);
+            $rightSound[0].play();
+            $result.text("Wow! You got it right!");
+            $score.text((score)+1);
+            $checkButton.css("display", "none");
+            $nextButton.css("display", "flex");
+        } else{
+            $("body").append($wrongSound);
+            $wrongSound[0].play();
+            $result.text("That's not the word we are looking for. Try again.");
+        }
+    });
+//NEXT BUTTON CLICK EVENT FUNCTION
+    $nextButton.on("click", function(){
+        e.preventDefault;
+        console.log("I'm Done");
+        $nextButton.css("display", "none");
+        $checkButton.css("display", "flex");
+        $newSpan.text(scram[genScramWord()]);
+        $result.text("");
+        $answerBox.val("");
+    })
